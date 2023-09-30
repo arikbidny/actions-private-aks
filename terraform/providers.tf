25863bfb82faa5ea7fcde14f284ce0cc2b1599ef
Arik Bidny <arikbidny@gmail.com>
2023-09-30 12:25:33 +0300

    configuring backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -19,5 +19,8 @@ terraform {
     storage_account_name = "bacloudterraformstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = "9a787734-ee5c-422e-ba06-938e14b79c31"
+    tenant_id            = "34a7b585-cb55-4d8e-9f2b-37560c2fa9b9"
   }
 }
