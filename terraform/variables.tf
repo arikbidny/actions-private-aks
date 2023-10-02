50c03ba470a1e8ba80ce2ec293422ebd93e46d7c
Arik Bidny <arikbidny@gmail.com>
2023-10-02 21:16:15 +0300

    Changing provider file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -158,7 +158,7 @@ variable "kubernetes_version" {
 
 variable "default_node_pool_vm_size" {
   description = "Specifies the vm size of the default node pool"
-  default     = "Standard_D8ads_v5"
+  default     = "Standard_D8s_v3"
   type        = string
 }
 
